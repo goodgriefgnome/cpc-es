c4445f87a2b0aaaeb19a0431b098a505c581c602
lijie.wong@gmail.com <lijie.wong@gmail.com>
2015-12-10 11:23:14 +1100

    Make the XMLSlideShow manipulation IO-agnostic.

--- src/main/java/au/org/cpc/es/CreatePptxServlet.java
+++ src/main/java/au/org/cpc/es/CreatePptxServlet.java
@@ -1,9 +1,7 @@
 package au.org.cpc.es;
 
 import java.io.IOException;
-import java.util.List;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.poi.xslf.usermodel.SlideLayout;
@@ -15,32 +13,33 @@ import org.apache.poi.xslf.usermodel.XSLFTextParagraph;
 import org.apache.poi.xslf.usermodel.XSLFTextRun;
 import org.apache.poi.xslf.usermodel.XSLFTextShape;
 
-public class CreatePptxServlet extends HttpServlet {
-    static private class MakeSlides {
-        private XMLSlideShow show;
-        private XSLFSlideLayout blankLayout;
-        private XSLFSlideLayout textLayout;
-        private XSLFSlide slide = null;
-        private boolean[] placeholderUsed = new boolean[2];
-
-        public MakeSlides(XMLSlideShow show) {
-            while (!show.getSlides().isEmpty()) {
-                show.removeSlide(0);
-            }
-            this.show = show;
-
-            List<XSLFSlideMaster> masters = show.getSlideMasters();
-            XSLFSlideMaster master = masters.get(masters.size() - 1);
-
-            this.blankLayout = master.getLayout(SlideLayout.BLANK);
-            this.textLayout = master.getLayout(SlideLayout.TEXT);
-        }
-
+public class CreatePptxServlet extends javax.servlet.http.HttpServlet {
+    static private class SlideWriter {
         private enum State {
             PAGE_BREAK,
             TITLE,
             BODY,
             CREDITS,
+        }
+
+        private XMLSlideShow show;
+        private XSLFSlideLayout blankLayout;
+        private XSLFSlideLayout textLayout;
+        private State state = State.PAGE_BREAK;
+        private XSLFSlide slide = null;
+        private boolean[] placeholderUsed = new boolean[2];
+
+        public SlideWriter(XMLSlideShow show) {
+            while (!show.getSlides().isEmpty()) {
+                show.removeSlide(0);
+            }
+            this.show = show;
+
+            java.util.List<XSLFSlideMaster> masters = show.getSlideMasters();
+            XSLFSlideMaster master = masters.get(masters.size() - 1);
+
+            this.blankLayout = master.getLayout(SlideLayout.BLANK);
+            this.textLayout = master.getLayout(SlideLayout.TEXT);
         }
 
         private void initSlide(XSLFSlide slide) {
@@ -50,48 +49,42 @@ public class CreatePptxServlet extends HttpServlet {
             }
         }
 
-        public void generate(java.io.BufferedReader reader) throws IOException {
-            State state = State.PAGE_BREAK;
-
-            for (;;) {
-                String line = reader.readLine();
-                if (line == null) break;
-                line = line.trim();
-                if (line.isEmpty()) {
-                    if (state == State.PAGE_BREAK) {
-                        show.createSlide(blankLayout);
-                    }
-                    state = State.PAGE_BREAK;
-                    continue;
-                }
-
+        public void processLine(String line) {
+            line = line.trim();
+            if (line.isEmpty()) {
                 if (state == State.PAGE_BREAK) {
-                    initSlide(show.createSlide(textLayout));
-                    state = State.BODY;
+                    show.createSlide(blankLayout);
                 }
-                if (line.equals("##title")) {
-                    state = State.TITLE;
-                    continue;
-                } else if (line.equals("##credits")) {
-                    state = State.CREDITS;
-                    addText(1, " ", 0.5);
-                    continue;
-                }
+                state = State.PAGE_BREAK;
+                return;
+            }
 
-                switch (state) {
-                case TITLE:
-                    addText(0, line, 1.0);
-                    state = State.BODY;
-                    break;
-                case BODY:
-                    addText(1, line, 1.0);
-                    break;
-                case CREDITS:
-                    addText(1, line, 0.5);
-                    break;
-                default:
-                    throw new IllegalStateException();
-                }
+            if (state == State.PAGE_BREAK) {
+                initSlide(show.createSlide(textLayout));
+                state = State.BODY;
+            }
+            if (line.equals("##title")) {
+                state = State.TITLE;
+                return;
+            } else if (line.equals("##credits")) {
+                state = State.CREDITS;
+                addText(1, " ", 0.5);
+                return;
+            }
+
+            switch (state) {
+            case TITLE:
+                addText(0, line, 1.0);
+                state = State.BODY;
+                break;
+            case BODY:
+                addText(1, line, 1.0);
+                break;
+            case CREDITS:
+                addText(1, line, 0.5);
+                break;
+            default:
+                throw new IllegalStateException();
             }
         }
 
@@ -126,7 +119,14 @@ public class CreatePptxServlet extends HttpServlet {
     @Override
     public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         XMLSlideShow show = new XMLSlideShow(new java.io.ByteArrayInputStream(pptxTemplate));
-        new MakeSlides(show).generate(req.getReader());
+        SlideWriter writer = new SlideWriter(show);
+        java.io.BufferedReader reader = req.getReader();
+        for (;;) {
+            String line = reader.readLine();
+            if (line == null) break;
+            writer.processLine(line);
+        }
+
         resp.setContentType("application/octet-stream");
         show.write(resp.getOutputStream());
     }
